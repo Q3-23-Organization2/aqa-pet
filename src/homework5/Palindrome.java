0f340c886352b9102bc12fae079cce10ac8c5d29
chygraiser <s.chygrai@gamil.com>
2023-10-04 17:59:51 +0200

    Task #1 simple update

--- src/homework5/Palindrome.java
+++ src/homework5/Palindrome.java
@@ -4,21 +4,19 @@ public class Palindrome {
     public static void main(String[] args) {
         int value = 0;
         String word = "PYLYP";
-        char str [] = word.toCharArray();
+        char[] string = word.toCharArray();
 
-
-        for (int i = 0; i < str.length; i++) {
-            if (str[i] != str[str.length - i - 1]) {
+        for (int i = 0; i < string.length; i++) {
+            if (string[i] != string[string.length - i - 1]) {
                 value = 1;
                 break;
             }
         }
-
         if (value == 0) {
             System.out.println(word + " is palindrome");
         } else
         {
-            System.out.println(word + "is not palindrome");
+            System.out.println(word + " is not palindrome");
         }
     }
 }
