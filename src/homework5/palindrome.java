1e1309d95af695aed749ea3354b23e22e34cd37b
chygraiser <s.chygrai@gamil.com>
2023-10-04 16:34:39 +0200

    test array

--- src/homework5/palindrome.java
+++ src/homework5/palindrome.java
@@ -1,4 +1,14 @@
 package homework5;
 
+
 public class palindrome {
+    public static void main(String[]args){
+        String [] strArray = new String[5];
+        strArray [0] = "Test 0";
+        strArray [1] = "Test 1";
+        strArray [2] = "Test 2";
+        strArray [3] = "Test 3";
+        System.out.println(strArray [3]);
+
+    }
 }
