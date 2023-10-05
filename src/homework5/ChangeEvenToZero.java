c8e25497fdb337f03e79e49b4e403ea84a6e6569
chygraiser <s.chygrai@gamil.com>
2023-10-05 19:43:13 +0200

    Task #3 ChangeEvenToZero

--- src/homework5/ChangeEvenToZero.java
+++ src/homework5/ChangeEvenToZero.java
@@ -2,16 +2,14 @@ package homework5;
 
 public class ChangeEvenToZero {
     public static void main(String[] args) {
-        int[] numbers = {23, 4, 12, 58, 93, 97, 113, 43};
+        int[] numbers = {23, 4, 12, 58, 93, 56, 113, 43};
 
-        for (int i = 0; i < numbers.length; ++i) {
+        for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] % 2 == 0) {
-                System.out.println(i = 0);
+                numbers[i] = 0;
             }
-            else {
-                System.out.println(numbers[i]);
-            }
-        }
 
+            System.out.println(numbers[i]);
+        }
     }
 }
