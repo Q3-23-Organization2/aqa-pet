03b22efd8a1dc398c40a0a9e93372c37adcab99b
chygraiser <s.chygrai@gamil.com>
2023-10-06 22:14:02 +0200

    Task #4 Sinus fix comments

--- src/homework5/Sinus.java
+++ src/homework5/Sinus.java
@@ -3,12 +3,15 @@ package homework5;
 import java.lang.Math;
 public class Sinus {
     public static void main(String[] args) {
-        int[] x = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360};
+        int array = 37;
+        double[] sinusArray = new double[array];
 
+        for (int i = 0, step = 0; i < array; i++, step+=10) {
+            double sinValue = Math.sin(step);
+            sinusArray[i] = sinValue;
 
-        for (int i = 0; i < x.length; i++) {
+            System.out.println(sinusArray[i]);
 
-            System.out.println(Math.sin(x[i]));
         }
 
     }
