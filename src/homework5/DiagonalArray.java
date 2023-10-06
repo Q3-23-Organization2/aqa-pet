cd8519392aae1294d602f2c3c133f48e69b3371c
chygraiser <s.chygrai@gamil.com>
2023-10-06 22:29:14 +0200

    Task #5 DiagonalArray

--- src/homework5/DiagonalArray.java
+++ src/homework5/DiagonalArray.java
@@ -3,13 +3,29 @@ package homework5;
 public class DiagonalArray {
     public static void main(String[] args) {
         int[][] twoDimArray = {{1, 13, 6, 7}, {-2, 12, 5, 7}, {3, -1, 9, 21}, {4, -12, -8, 14}};
+        int count = 0;
+        int sum = 0;
+        int n = twoDimArray.length;
 
-        for (int i = 0; i < twoDimArray.length; i++) {
-            for (int j = 0; j < twoDimArray.length; j++) {
-                if (i == j) {
-                    System.out.print(twoDimArray[i][j] + ", ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (twoDimArray[i][j] < 0) {
+                    count++;
                 }
+
             }
         }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (twoDimArray[i][j] % 2 != 0) {
+                    sum += twoDimArray[i][j];
+
+                }
+
+            }
+        }
+        System.out.println("Count of negative numbers: " + count);
+        System.out.println("Sum of odd numbers: " + sum);
     }
 }
