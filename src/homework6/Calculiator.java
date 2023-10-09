2859f0708a3449ba2d5804bd584fe9fd13d2b167
chygraiser <s.chygrai@gamil.com>
2023-10-09 15:30:19 +0200

    Task #2 Calculiator done

--- src/homework6/Calculiator.java
+++ src/homework6/Calculiator.java
@@ -6,14 +6,37 @@ public class Calculiator {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Hello in the Calculiator program, please enter your number 1 and number 2");
+
+        System.out.println("Please enter your first number: ");
         double value1 = scan.nextDouble();
+
+        Scanner scan2 = new Scanner(System.in);
+        System.out.println("Please enter operation(+,-,/,*): ");
+        String operation = scan2.next();
+
+        System.out.println("Please enter your second number: ");
         double value2 = scan.nextDouble();
 
-
-
-
-
+        if (operation.equals("+"))
+        {
+            System.out.println("Your answer is: "+(value1 + value2));
+        }
+        else if (operation.equals("-"))
+        {
+            System.out.println("Your answer is: "+(value1 - value2));
+        }
+        else if (operation.equals("*"))
+        {
+            System.out.println("Your answer is: "+(value1 * value2));
+        }
+        else if (operation.equals("/"))
+        {
+            System.out.println("Your answer is: "+(value1 / value2));
+        }
+        else
+        {
+            System.out.println("Sorry, your operation is incorrect. \nPlease try again.");
+        }
     }
 
 }
