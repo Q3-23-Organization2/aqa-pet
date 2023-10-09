f094f023414449219b7a159d727a7c8c9b58f080
chygraiser <s.chygrai@gamil.com>
2023-10-09 11:56:13 +0200

    Task #2 Calculiator update

--- src/homework6/Calculiator.java
+++ src/homework6/Calculiator.java
@@ -1,7 +1,18 @@
 package homework6;
 
+import java.util.Scanner;
+
 public class Calculiator {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello in the Calculiator program, please enter your number 1 and number 2");
+        double value1 = scan.nextDouble();
+        double value2 = scan.nextDouble();
+
+
+
+
 
     }
 
