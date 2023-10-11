341f08ecbdacbef7f12e650d470bddeaa6839365
chygraiser <s.chygrai@gamil.com>
2023-10-11 21:49:10 +0200

    Task #2 Calculiator done

--- src/homework6/Calculiator.java
+++ src/homework6/Calculiator.java
@@ -1,42 +1,43 @@
 package homework6;
 
-import java.util.Scanner;
-
 public class Calculiator {
-    public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+    public int value1;
 
-        System.out.println("Please enter your first number: ");
-        double value1 = scan.nextDouble();
-
-        Scanner scan2 = new Scanner(System.in);
-        System.out.println("Please enter operation(+,-,/,*): ");
-        String operation = scan2.next();
-
-        System.out.println("Please enter your second number: ");
-        double value2 = scan.nextDouble();
-
-        if (operation.equals("+"))
-        {
-            System.out.println("Your answer is: "+(value1 + value2));
-        }
-        else if (operation.equals("-"))
-        {
-            System.out.println("Your answer is: "+(value1 - value2));
-        }
-        else if (operation.equals("*"))
-        {
-            System.out.println("Your answer is: "+(value1 * value2));
-        }
-        else if (operation.equals("/"))
-        {
-            System.out.println("Your answer is: "+(value1 / value2));
-        }
-        else
-        {
-            System.out.println("Sorry, your operation is incorrect. \nPlease try again.");
-        }
+    public void setValue1(int enterValue1) {
+        value1 = enterValue1;
     }
+
+    public int value2;
+
+    public void setValue2(int enterValue2) {
+        value2 = enterValue2;
+    }
+    public int plus;
+
+    public void operationPlus() {
+        plus = value1 + value2;
+        System.out.println("Value1 + Value2 = " + plus);
+    }
+    public int minus;
+
+    public void operationMinus() {
+        minus = value1 - value2;
+        System.out.println("Value1 - Value2 = " + minus);
+    }
+
+    public int divide;
+
+    public void operationDivide() {
+        divide = value1 / value2;
+        System.out.println("Value1 / Value2 = " + divide);
+    }
+    public int multi;
+
+    public void operationMultiple() {
+        multi = value1 * value2;
+        System.out.println("Value1 * Value2 = " + multi);
+    }
+
 
 }
