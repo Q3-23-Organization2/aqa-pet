171215e24b74c1ffa78bba26dc396ae4536b17d5
chygraiser <s.chygrai@gamil.com>
2023-10-31 21:23:16 +0100

    HomeWork 10 Task2 -> resolve by sorting and List

--- src/homework10/Task2/Main.java
+++ src/homework10/Task2/Main.java
@@ -1,36 +1,45 @@
 package homework10.Task2;
 
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int[] num = {764, -17, 78, 43, 90, -894, 0, -44, 1111, 13, 154, 139};
 
-        for (int i = 0; i < num.length; i++) {
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(-67);
+        integerList.add(43);
+        integerList.add(13);
+        integerList.add(-99);
+        integerList.add(0);
+        integerList.add(413);
+        integerList.add(144443);
+        integerList.add(-99);
+        integerList.add(10);
 
-            System.out.println(num[i]);
-        }
-
-        System.out.print("Please type a number: ");
+        Collections.sort(integerList);
+        System.out.println(integerList);
+        System.out.print("Please enter a number: ");
 
         int x = scan.nextInt();
+        List<Integer> integerList2 = new ArrayList<>();
 
-        System.out.println("Numbers below X: ");
-
-        for (int i = 0; i < num.length; i++) {
-            if (num[i] < x) {
-                System.out.println(num[i]);
-            }
-
-        }
-
-        System.out.println("Numbers above X: ");
-
-        for (int i = 0; i < num.length; i++) {
-            if (num[i] > x) {
-                System.out.println(num[i]);
+        for (int n: integerList) {
+            if (n < x) {
+                integerList2.add(n);
             }
         }
+        System.out.println("Numbers below " + x + ": " + integerList2);
+
+        List<Integer> integerList3 = new ArrayList<>();
+
+        for (int n: integerList) {
+            if (n > x) {
+                integerList3.add(n);
+            }
+        }
+        System.out.println("Numbers above " + x + ": " + integerList3);
     }
 }
 
